03ff719591662d5b852c904a45d939ac699f792a
Muhammad Ibad <mibad0338@gmail.com>
2022-10-28 22:15:03 +0500

    db connected

---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express()
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const cors = require('cors')
+require('dotenv/config')
+
+
+
+
+const port = process.env.PORT || 3000
+
+//middleware
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(cors('*'))
+
+const mongodbURL = process.env.DB_URL
+console.log(mongodbURL,"mongodbURl")
+mongoose.connect(mongodbURL,
+    { useUnifiedTopology: true, useNewUrlParser: true }).then(() => console.log('connected'));
+
+// run server
+app.listen(port, () => {
+    console.log(`Server starts at ${port}`)
+})
+
+
+
+// error handling
+app.use((res, req, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error)
+})
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
